db508d2e73de4fafd59f5979dbbc44e77364dd72
Le Ngoc Thanh <thanhlengoc21@gmail.com>
2018-09-10 22:40:02 +0700

    add package body-parser to send request

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+app.post('/', (req, res) => {
+    if(req.body.username=="xxx" && req.body.password=="cccc")
+        res.send("login success!");
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
